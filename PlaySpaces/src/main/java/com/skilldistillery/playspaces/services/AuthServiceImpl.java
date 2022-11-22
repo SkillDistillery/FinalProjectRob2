01519741f50a73aa5aa67b3c786009ae41e30394
Rob Roselius <roselius@skilldistillery.com>
2022-11-22 13:21:20 -0700

    Full-stack smoke test

--- PlaySpaces/src/main/java/com/skilldistillery/playspaces/services/AuthServiceImpl.java
+++ PlaySpaces/src/main/java/com/skilldistillery/playspaces/services/AuthServiceImpl.java
@@ -5,30 +5,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.playspaces.entities.User;
+import com.skilldistillery.playspaces.repositories.UserRepository;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 	
-//	@Autowired
-//	private UserRepository userRepo;
+	@Autowired
+	private UserRepository userRepo;
 	
 	@Autowired
 	private PasswordEncoder encoder;
 
 	@Override
 	public User register(User user) {
-//		String encryptedPassword = encoder.encode(user.getPassword());
-//		user.setPassword(encryptedPassword);
-//		user.setEnabled(true);
-//		user.setRole("standard");
-//		userRepo.saveAndFlush(user);
+		String encryptedPassword = encoder.encode(user.getPassword());
+		user.setPassword(encryptedPassword);
+		user.setEnabled(true);
+		user.setRole("standard");
+		userRepo.saveAndFlush(user);
 		return user;
 	}
 
 	@Override
 	public User getUserByUsername(String username) {
-//		return userRepo.findByUsername(username);
-		return null; //FIXME
+		return userRepo.findByUsername(username);
 	}
 
 }
