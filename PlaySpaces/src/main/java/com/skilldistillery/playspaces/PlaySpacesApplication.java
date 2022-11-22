1b02727c708cb2a4f4102376dee50cf1c4e57852
Rob Roselius <roselius@skilldistillery.com>
2022-11-22 10:50:52 -0700

    Create Boot/REST project, connect project dependency

---
+++ PlaySpaces/src/main/java/com/skilldistillery/playspaces/PlaySpacesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.playspaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaySpacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaySpacesApplication.class, args);
+	}
+
+}
---
+++ PlaySpaces/src/test/java/com/skilldistillery/playspaces/PlaySpacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.playspaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaySpacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
