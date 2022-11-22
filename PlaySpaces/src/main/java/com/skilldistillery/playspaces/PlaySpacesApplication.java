3036b3285259e969592b0cbe5488ff7769dcee36
Rob Roselius <roselius@skilldistillery.com>
2022-11-22 11:00:05 -0700

    Stub Spring Security config

--- PlaySpaces/src/main/java/com/skilldistillery/playspaces/PlaySpacesApplication.java
+++ PlaySpaces/src/main/java/com/skilldistillery/playspaces/PlaySpacesApplication.java
@@ -2,6 +2,9 @@ package com.skilldistillery.playspaces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PlaySpacesApplication {
@@ -9,5 +12,9 @@ public class PlaySpacesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PlaySpacesApplication.class, args);
 	}
-
+	
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+	   return new BCryptPasswordEncoder();
+	}
 }
